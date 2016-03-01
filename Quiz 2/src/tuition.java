c01daea77e9d30ea53a1565a527edc484b60a142
Bonard Geyer <bgeyer@udel.edu>
2016-02-29 20:52:40 -0500

    initial commit

---
+++ Quiz 2/src/tuition.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class tuition {
+	public void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		double initTuition;
+		double percentInc;
+		double repayAPR;
+		double term;
+		
+		System.out.println("What is your initial tuition?");
+		initTuition=input.nextDouble();
+		System.out.println("What is yearly percent increase in tuition?");
+		percentInc=input.nextDouble();
+		System.out.println("What is your repayment APR?");
+		repayAPR=input.nextDouble();
+		System.out.println("How many terms are you attending?");
+		term=input.nextDouble();
+	}
+	
+	public double 
+
+}
