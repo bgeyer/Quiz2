ccab6717b2303e4069445a7ac48f76ee2d96619b
Bonard Geyer <bgeyer@udel.edu>
2016-03-01 00:29:47 -0500

    finish

--- Quiz2/src/main/java/base/TuitionCalculator.java
+++ Quiz2/src/main/java/base/TuitionCalculator.java
@@ -1,5 +1,32 @@
 package base;
+import java.util.Scanner;
+import org.apache.poi.ss.formula.functions.FinanceLib;
 
 public class TuitionCalculator {
+	public void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		
+		double initTuition;
+		double percentInc;
+		double repayAPR;
+		double pmt;
+		double fullTuition;
+		int term;
+		
+		System.out.println("What is your tuition payment per semester?");
+		initTuition=input.nextDouble();
+		System.out.println("By what percent does tuition increase per year?");
+		percentInc=input.nextDouble();
+		System.out.println("What is your repayment APR?");
+		repayAPR=input.nextDouble();
+		System.out.println("In how many years will you pay off your loan?");
+		term=input.nextInt();
+		fullTuition = initTuition;
+		
+		for(int i=1; i<9; i++) {
+			fullTuition = fullTuition * percentInc;
+		}
 	
+		pmt = FinanceLib.pmt(repayAPR, term, fullTuition, 0.0, false);
+	}
 }
